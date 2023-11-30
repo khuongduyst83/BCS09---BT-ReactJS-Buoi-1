5e860b320a2f3bc999b0dd64bd77b6006c6c2054
Khuong_Duy <nguyenhoangkhuongduy2017vta@gmail.com>
2023-11-30 17:01:44 +0700

    update bt react b1

---
+++ src/component/BaiTapThucHanhLayOut/Body.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+// import "./style.scss";
+export default class Body extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="banner">
+            <h1>A warm welcome!</h1>
+            <p>
+              Bootstrap utility classes are used to create this jumbotron since
+              the old component has been removed from the framework. Why create
+              custom CSS when you can use utilities?
+            </p>
+            <button>Call to action</button>
+          </div>
+          <div className="content container text-center">
+            <div className="row g-2">
+              <div className="col-4">
+                <div className="p-3">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn btn-primary">
+                        Go somewhere
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="p-3">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn btn-primary">
+                        Go somewhere
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="p-3">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn btn-primary">
+                        Go somewhere
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="p-3">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn btn-primary">
+                        Go somewhere
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="p-3">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn btn-primary">
+                        Go somewhere
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="p-3">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a href="#" className="btn btn-primary">
+                        Go somewhere
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
